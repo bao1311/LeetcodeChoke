b22e355d569abdf090c86fde84a115daeeadec1e
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-04-19 13:43:03 -0700

    new update

---
+++ sidepanel.js
@@ -0,0 +1,5 @@
+document.getElementById("start-timer").addEventListener("click", () => {
+    document.getElementById("status").innerText = "⏳ Focus mode started...";
+    // TODO: Add timer logic here
+  });
+  
