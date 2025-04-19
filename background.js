8535af89f6175c7fa2a2bd1bfd10dd364ebe5163
_HDH <45970570+hdh06@users.noreply.github.com>
2025-04-19 13:51:39 -0700

    Update to open side panel directly

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.action.onClicked.addListener((tab) => {
+    chrome.sidePanel.setOptions({
+      tabId: tab.id,
+      path: 'sidepanel.html',
+      enabled: true
+    });
+    chrome.sidePanel.open({ tabId: tab.id });
+  });
+  
