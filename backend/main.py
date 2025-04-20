32523e2deff1ebb2fb972d53d78e7b235a66ba7c
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-04-20 07:02:31 -0700

    Change file structure

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500"
+        "*"
     ],
     allow_credentials = True,
     allow_methods = ["*"],
